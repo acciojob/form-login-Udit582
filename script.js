24701f5fbbe105895a6849ddcaba75edcc90e7e6
Udit582 <uditpaliwal582@gmail.com>
2025-11-20 12:41:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
+function getFormvalue(event) {
+    event.preventDefault(); // stops page from refreshing
 
- function getFormValue() {
     const a = document.getElementsByName("fname")[0].value;
     const b = document.getElementsByName("lname")[0].value;
 
-    // When submit button is clicked
-    document.getElementById("submit").addEventListener("click", function() {
-        alert(a + " " + b);
-    });
+    alert(a + " " + b);
 }
-
-
