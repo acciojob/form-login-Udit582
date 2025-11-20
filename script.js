a4793f246768306e30d0faed0a3ab747ba7a99f8
Udit582 <uditpaliwal582@gmail.com>
2025-11-20 12:39:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function getFormvalue() {
-    //Write your code here
 
+ function getFormValue() {
+    const a = document.getElementsByName("fname")[0].value;
+    const b = document.getElementsByName("lname")[0].value;
+
+    // When submit button is clicked
+    document.getElementById("submit").addEventListener("click", function() {
+        alert(a + " " + b);
+    });
 }
+
+
